d69bd3626f6ede13b1a43b79e5fd9a0c3766bb10
Abuchi Kingsley <abuchikingsley76@gmail.com>
2019-10-19 09:47:02 +0100

    ft: Initial commit, headers component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-hd',
+    templateUrl: './header.component.html',
+    styleUrls:['./header.component.css']
+})
+export class HeaderComponent{
+    title: 'Mini-Netflix'
+}
