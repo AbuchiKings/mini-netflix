8dc86faeee35a43e7100fe16af384fff1e68c109
Abuchi Kingsley <abuchikingsley76@gmail.com>
2019-10-25 13:04:26 +0100

    Fix routresolver issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import { slideInAnimation } from './app.animation';
 
 @Component({
   selector: 'app-root',
   template: `
   <app-hd></app-hd>
-  <main>
-    <router-outlet></router-outlet>
+  <main   [@slideInAnimation]="o.isActive ? o.activatedRoute : '' ">
+    <router-outlet #o ="outlet"></router-outlet>
   </main>
-  `
+  `,
+  animations: [slideInAnimation]
 })
 export class AppComponent {
   title = 'Mini-Netflix';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { RouterModule, Routes } from '@angular/router';
@@ -51,6 +52,7 @@ import { MovieListResolver } from './services/movieList-resolver.service';
 
     ]),
     HttpClientModule,
+    BrowserAnimationsModule
 
 
   ],
